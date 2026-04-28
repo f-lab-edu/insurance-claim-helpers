fb24923e23fd8033603fa93ff456d4e27f7a6801
소완규 <sowankyu@sowangyuui-MacBookAir.local>
2026-04-28 22:07:07 +0900

    스프링 프로젝트 생성

---
+++ src/main/java/com/swk/claimhelpers/InsuranceClaimHelpersApplication.java
@@ -0,0 +1,13 @@
+package com.swk.claimhelpers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceClaimHelpersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsuranceClaimHelpersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swk/claimhelpers/InsuranceClaimHelpersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swk.claimhelpers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceClaimHelpersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
